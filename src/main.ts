49d18daa0dce984cfebf9b5b9906488dce46d5ce
Yuri Gomes <yuurig@gmail.com>
2022-06-21 22:46:00 -0300

    chore: setup cors and version

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,24 @@ import {
 } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { configurations } from './config/environment';
+import { VersioningType } from '@nestjs/common';
+import fastifyCors from '@fastify/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter({
-      logger: false,
+      logger: true,
     })
   );
 
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  })
+
+  // app.enableCors();
+  await app.register(fastifyCors)
   await app.listen(configurations.PORT);
 }
 bootstrap();
